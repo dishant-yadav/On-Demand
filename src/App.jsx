61d19d86da7113d6a8219656f55615d2f931c7df
Dishant Yadav <dishantyadav599@gmail.com>
2023-02-17 16:36:37 +0530

    Created the hero page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { Anchor, MantineProvider, Text } from "@mantine/core";
 import { Link } from "react-router-dom";
+import Hero from "./pages/hero";
 
 export default function App() {
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS>
-      <Text>Welcome to OnDemand!</Text>
-      <Link to={"/signup"}>SignUp</Link>
-      <Link to={"/login"}>Login</Link>
+      <Hero />
     </MantineProvider>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App";
 import ErrorPage from "./error-page";
 import Login from "./pages/login";
 import SignUp from "./pages/signup";
-import Home from "./pages/home";
+import Home from "./pages/hero";
 
 const router = createBrowserRouter([
   {
